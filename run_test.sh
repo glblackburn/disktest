31e84090fab49ee0ee99ac9ed31ad6e6712e429d
Lee Blackburn <g.lee.blackburn@gmail.com>
2018-03-03 12:14:43 -0500

    add remote test support and reduce output with verbose option

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# monitor run
+# while (true) ; do find . -type d -name "disktest*" -exec ls -ld {} \; ; sleep 1 ; done
+#
 TEST_DIR="disktest.$$"
 
 DATETIME=`date "+%Y-%m-%d_%H%M%S"`
@@ -14,61 +18,114 @@ function usage {
 #    echo "  --host                  Display this help message."
 }
 
-function makeTestDir {
-    echo "make ${TEST_PATH}"
-    mkdir ${TEST_PATH}
-    if [[ ! -e ${TEST_PATH} ]] ; then
-	echo "Could not create test path: ${TEST_PATH}"
-	exit 10
+function runCommand {
+    cmd=$1
+    if [ "$VERBOSE" = true ] ; then
+	echo "cmd=[${cmd}]"
     fi
-    ls -la ${TEST_PATH}
+    if [ $IS_LOCAL ] ; then
+	$cmd
+    else
+	if [ "$VERBOSE" = true ] ; then
+	    echo "ssh ${REMOTE_USER}@${REMOTE_HOST} \"$cmd\""
+	fi
+	ssh ${REMOTE_USER}@${REMOTE_HOST} "$cmd"
+    fi
+}
+
+function testStat {
+    if [ $IS_LOCAL ] ; then
+	stat ${TEST_PATH} > /dev/null 2>&1
+    else
+	runCommand "stat ${TEST_PATH} > /dev/null 2>&1"
+    fi
+}
+
+function makeTestDir {
+    echo "makeTestDir"
+    runCommand "mkdir ${TEST_PATH}"
+
+    if ! testStat ${TEST_PATH} ; then
+	echo "Could not create test path: ${TEST_PATH}"
+	exit 11
+    fi
+    if [ "$VERBOSE" = true ] ; then
+	runCommand "hostname"
+	runCommand "ls -la ${TEST_PATH}"
+    fi
 }
 
 function copyScriptToTestDir {
-    echo "copy ${TEST_SCRIPT} to ${TEST_PATH}"
-    cp ${TEST_SCRIPT} ${TEST_PATH}
-    if [[ ! -e ${TEST_PATH}/${TEST_SCRIPT} ]] ; then
-	echo "Could not copy test script (${TEST_SCRIPT}) to test path: ${TEST_PATH}"
-	exit 11
+    echo "copyScriptToTestDir"
+    if [ $IS_LOCAL ] ; then
+	cp ${TEST_SCRIPT} ${TEST_PATH}
+    else
+	scp -q ${TEST_SCRIPT} ${REMOTE_USER}@${REMOTE_HOST}:${TEST_PATH}
     fi
-    ls -la ${TEST_PATH}
+    if ! testStat "${TEST_PATH}/${TEST_SCRIPT}" ; then
+	echo "Could not copy test script (${TEST_SCRIPT}) to test path: ${TEST_PATH}"
+	exit 20
+    fi
+    if [ "$VERBOSE" = true ] ; then
+	runCommand "ls -la ${TEST_PATH}"
+    fi
 }
 
 function runTest {
-    echo "run ${TEST_PATH}/${TEST_SCRIPT}"
-    CPATH=`pwd`
-    cd ${TEST_PATH}
-    ./${TEST_SCRIPT} > ${TEST_LOG}
-    ls -la ${TEST_PATH}
-    cat ${TEST_LOG}
-    cd ${CPATH}
+    echo "runTest"
+    
+    if [ $IS_LOCAL ] ; then
+	CPATH=`pwd`
+	cd ${TEST_PATH}
+	./${TEST_SCRIPT} > ${TEST_LOG}
+	cd ${CPATH}
+	if [ "$VERBOSE" = true ] ; then
+	    ls -la ${TEST_PATH}
+	    cat ${TEST_PATH}/${TEST_LOG}
+	fi
+    else
+    	runCommand "cd ${TEST_PATH} && ./${TEST_SCRIPT} > ${TEST_LOG}"
+	if [ "$VERBOSE" = true ] ; then
+	    runCommand "ls -la ${TEST_PATH}"
+	    runCommand "cat ${TEST_PATH}/${TEST_LOG}"
+	fi
+    fi
 }
 
 function pullLog {
-    echo "copy ${TEST_PATH}/${TEST_LOG} to ${LOG_DIR}"
+    echo "pullLog"
     mkdir -p ${LOG_DIR}
     if [[ ! -d ${LOG_DIR} ]] ; then
 	echo "Log dir does not exist: ${LOG_DIR}"
-	exit 12
+	exit 30
     fi
 
-    cp "${TEST_PATH}/${TEST_LOG}" ${LOG_DIR}
-    ls -la  ${LOG_DIR}
+    if [ $IS_LOCAL ] ; then
+	cp "${TEST_PATH}/${TEST_LOG}" ${LOG_DIR}
+    else
+	scp -q "${REMOTE_USER}@${REMOTE_HOST}:${TEST_PATH}/${TEST_LOG}" ${LOG_DIR}
+    fi
+    if [ "$VERBOSE" = true ] ; then
+	ls -la  ${LOG_DIR}
+    fi
 }
 
 function showLog {
-    echo "show ${LOG_DIR}/${TEST_LOG}"
+    echo "showLog"
     cat ${LOG_DIR}/${TEST_LOG}
 }
 
 function cleanUp {
-    echo "delete ${TEST_PATH}"
-    rm -r ${TEST_PATH}
-    if [[ -e ${TEST_PATH} ]] ; then
+    echo "cleanUp"
+    runCommand "rm -r ${TEST_PATH}"
+    if testStat ${TEST_PATH} ; then
 	echo "Could not delete test path: ${TEST_PATH}"
-	exit 13
+	exit 41
     fi
-    ls -la ${TEST_PATH}
+    if [ "$VERBOSE" = true ] ; then
+	runCommand "hostname"
+	runCommand "ls -la ${TEST_PATH}"
+    fi
 }
 
 ################################################################################
@@ -104,14 +161,19 @@ function cleanUp {
 # get command line options
 ################################################################################
 VERBOSE=false
-while getopts ":hHu::v" opt; do
+while getopts ":hH:u:v" opt; do
 #    echo "*** HI! ***"
     case ${opt} in
 	u )
-	    USER=$OPTARG
+	    REMOTE_USER=$OPTARG
+	    echo "found -u $OPTARG"
 	    ;;
 	H )
-	    HOST=$OPTARG
+	    REMOTE_HOST=$OPTARG
+	    echo "found -H $OPTARG"
+	    ;;
+	u )
+	    REMOTE_USER=$OPTARG
 	    ;;
 	v )
 #	    echo "*** HI! ***"
@@ -131,6 +193,8 @@ done
 shift $((OPTIND -1))
 DIR=$1
 
+LOCAL_HOST=`hostname`
+
 ################################################################################
 # validate parameters
 ################################################################################
@@ -138,27 +202,46 @@ if [ ! $DIR ] ; then
     echo "missing path: set to current dir"
     DIR=`pwd`
 fi
-if [ ! $HOST ] ; then
+if [ ! $REMOTE_HOST ] ; then
     echo "missing host: set to local hostname"
-    HOST=`hostname`
+    REMOTE_HOST=$LOCAL_HOST
+else
+    if [ ! $REMOTE_USER ] ; then
+	echo "missing user"
+	usage
+	exit 2
+    else
+	echo "test connection to remote host"
+	if ! ssh "${REMOTE_USER}@${REMOTE_HOST}" 'hostname ; pwd' ; then
+	    echo "trouble connecting to remote host: ${REMOTE_USER}@${REMOTE_HOST}"
+	    exit 3
+	fi
+    fi
+fi
+
+if [ $LOCAL_HOST = $REMOTE_HOST ] ; then
+    IS_LOCAL=true
 fi
 
 TEST_PATH="${DIR}/${TEST_DIR}"
 TEST_SCRIPT="test_disk_speed.sh"
-TEST_LOG="disktest_${HOST}_${DATETIME}.txt"
+TEST_LOG="disktest_${REMOTE_HOST}_${DATETIME}.txt"
 LOG_DIR="`pwd`/log"
 
 ################################################################################
 # dump environment vars
 ################################################################################
 if [ "$VERBOSE" = true ] ; then
-	echo "HOST=[${HOST}]"
-	echo "DIR=[${DIR}]"
-	echo "TEST_DIR=[${TEST_DIR}]"
-	echo "TEST_PATH=[${TEST_PATH}]"
-	echo "TEST_SCRIPT=[${TEST_SCRIPT}]"
-	echo "TEST_LOG=[${TEST_LOG}]"
-	echo "LOG_DIR=[${TEST_DIR}]"
+    echo "IS_LOCAL=[$IS_LOCAL]"
+    echo "LOCAL_HOST=[$LOCAL_HOST]"
+    echo "REMOTE_HOST=[${REMOTE_HOST}]"
+    echo "REMOTE_USER=[${REMOTE_USER}]"
+    echo "DIR=[${DIR}]"
+    echo "TEST_DIR=[${TEST_DIR}]"
+    echo "TEST_PATH=[${TEST_PATH}]"
+    echo "TEST_SCRIPT=[${TEST_SCRIPT}]"
+    echo "TEST_LOG=[${TEST_LOG}]"
+    echo "LOG_DIR=[${TEST_DIR}]"
 fi
 ################################################################################
 # main code
